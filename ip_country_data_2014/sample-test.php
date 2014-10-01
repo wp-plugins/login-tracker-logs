73d06d5af046d99b0fb34ef834034df4fd90d797
selnomeria <selnomeria@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-01 09:17:13 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/login-tracker-logs/trunk@999671 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ip_country_data_2014/sample-test.php
@@ -0,0 +1,7 @@
+<?php
+include( dirname(__file__)."/geoip.inc" );
+$gi = geoip_open(dirname(__file__)."/GeoIP.dat", GEOIP_STANDARD); 
+$country_name = geoip_country_name_by_addr($gi, $user_ip );
+geoip_close($gi);
+//if ($country_name == 'Germany') { echo "You are from Germanyy";}
+?>
