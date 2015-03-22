a82588923232187d7ea38cf5ed98cfe1a695718c
selnomeria <selnomeria@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-22 22:07:00 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/login-tracker-logs/trunk@1118276 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- login-tracker-logs.php
+++ login-tracker-logs.php
@@ -14,14 +14,13 @@ class Login_Restrict_logs
 	protected $whois_site		='http://www.whois.com/whois/';
 	protected $StartSYMBOL		='<?php //';
 	
-	public function __construct()
-	{
-	register_activation_hook( __FILE__,  array($this, 'lgs_install'));
-	register_deactivation_hook( __FILE__,  array($this, 'lgs_uninstall'));
-	// run it before the headers and cookies are sent
-	add_action( 'after_setup_theme', array($this, 'lgs_login_checker'));
-	//add page under SETTINGS
-	add_action('admin_menu', array($this, 'logintrackss_funcct') ); 
+	public function __construct()	{
+		register_activation_hook( __FILE__,  array($this, 'lgs_install'));
+		register_deactivation_hook( __FILE__,  array($this, 'lgs_uninstall'));
+		// run it before the headers and cookies are sent
+		add_action( 'after_setup_theme', array($this, 'lgs_login_checker'));
+		//add page under SETTINGS
+		add_action('admin_menu', array($this, 'logintrackss_funcct') ); 
 	}
 	
 	
@@ -41,41 +40,26 @@ class Login_Restrict_logs
 			)  ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ; ") or die("error_2345_". mysql_error().$logintracks_tname);
 	}
 
-	public function lgs_uninstall()
-	{
-		unlink($this->allowed_ipss_file());
-	}	
+	public function lgs_uninstall()	{    unlink($this->allowed_ipss_file());    }	
 	
-	public function domainn()
-	{
-		return str_replace('www.','', $_SERVER['HTTP_HOST']);
-	}	
+	public function domainn()		{   return str_replace('www.','', $_SERVER['HTTP_HOST']);    }	
 	
-	public function validate_pageload($value, $action_name)
-	{
-		if ( 	!isset($value) || !wp_verify_nonce($value, $action_name) ) {
+	public function validate_pageload($value, $action_name){
+		if ( !isset($value) || !wp_verify_nonce($value, $action_name) ) {
 			die("not allowed due to interal_error_151");
 		}
 	}	
 	
-	public function get_remote_data($url, $from_mobile=false , $post_request=false, $post_paramtrs=false )	
-	{
+	public function get_remote_data($url, $from_mobile=false , $post_request=false, $post_paramtrs=false )	{
 		$c = curl_init();
 		curl_setopt($c, CURLOPT_URL, $url);
 		curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)");
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($c, CURLOPT_SSL_VERIFYHOST,false);
-		curl_setopt($c, CURLOPT_SSL_VERIFYPEER,false);
-		curl_setopt($c, CURLOPT_MAXREDIRS, 10);
-		curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 9);
-		$data = curl_exec($c);
-		curl_close($c);
-		return $data;
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);		curl_setopt($c, CURLOPT_SSL_VERIFYHOST,false);	curl_setopt($c, CURLOPT_SSL_VERIFYPEER,false);
+		curl_setopt($c, CURLOPT_MAXREDIRS, 10);			curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);		curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 9);
+		$data = curl_exec($c);							curl_close($c);		return $data;
 	}
 	
-	public function allowed_ipss_file()
-	{
+	public function allowed_ipss_file() 	{
 		//initial values
 		$bakcup_of_ipfile = get_option("backup_allowed_ips_login_". $this->domainn() );
 		$Value = !empty($bakcup_of_ipfile)?  $bakcup_of_ipfile : $this->StartSYMBOL. '101.101.101.101 (its James, my friend)|||102.102.102.102(its my pc),|||::1 (my windows address)||| and so on...';
@@ -87,20 +71,16 @@ class Login_Restrict_logs
 	}
 
 
-	public function lgs_login_checker() 
-	{
+	public function lgs_login_checker() {
 		//====================only when WP-LOGIN.PAGE is accessed. otherwise, the function will slower all normal page loads=======
 		//check if he is disabled
-		if (strpos($_SERVER['REQUEST_URI'],'/wp-login.php') !== false )
-		{
+		if (strpos($_SERVER['REQUEST_URI'],'/wp-login.php') !== false )	{
 			//variables for IP validity checking
 			$allwd_ips_content = file_get_contents($this->allowed_ipss_file());
 		
 			//check if BLOCKED
-			if (strpos($allwd_ips_content, $_SERVER['REMOTE_ADDR']) === false)
-			{
-				if (get_option('optin_for_white_ipss') == 3)
-				{
+			if (strpos($allwd_ips_content, $_SERVER['REMOTE_ADDR']) === false)	{
+				if (get_option('optin_for_white_ipss') == 3)	{
 					die("Login is disabled. Your IP is: ". $_SERVER['REMOTE_ADDR']);
 				}
 			}
@@ -181,15 +161,12 @@ class Login_Restrict_logs
 	{
 		global $wpdb;	$table_name = $wpdb->prefix . "restrictor_logins";
 		//if records cleared
-		if ($_POST['logintracks_clear']=='true') 
-		{
+		if ($_POST['logintracks_clear']=='true') {
 			$this->validate_pageload($_POST['update_nonce'],'lo_clear');
-			
 			$wpdb->get_results("DELETE FROM ".$table_name." WHERE success='0' OR success='1'");
 		}
 		//if changed whois
-		if (!empty($_POST['country_detct'])) 
-		{
+		if (!empty($_POST['country_detct'])) {
 			update_option('lgs_enable_WHOIS',$_POST['country_detct']);
 		}
 
@@ -204,36 +181,7 @@ class Login_Restrict_logs
 		.my_login_tracks tr.failed{ background-color:#FFC8BF;}
 		</style>
 		
-		<div class="my_login_tracks">
-			<h2>All logins:</h2>
-			<table class="widefat" cellpadding="3" cellspacing="3"><tr><th>Username</th><th>Time(server)</th><th>IP</th><th>COUNTRY (<a href="javascript:alert('This is just an approximate country name. To view the full info for a particular IP, then in this column, click that COUNTRY NAME and you will be redirected to the WHOIS WEBSITE, where you will see the FULL INFORMATION of that IP.');">Read THIS!!</a>)</th><th>Succeed</th></tr>
-			<?php
-			if ($results){
-				foreach ($results as $e) {
-						if(!empty($e->country))	 {$countryyy =  '<a href="'. $this->whois_site . $e->IP.'" target="_blank"> '.$e->country.'</a>';}
-						else 					 {$countryyy =  '<a href="'. $this->whois_site . $e->IP.'" target="_blank"> problem_54_from_plugin</a>';}
-		
-					echo '<tr class="succeed"><td>'.$e->username.'</td><td>'.$e->time.'</td><td>'.$e->IP.'</td><td>'.$countryyy.'</td><td>succeed<td></tr>';
-				}
-			}
-			?>
-			</table>
-			
-			
-			<!-- clear records -->
-			<form method="post" action="">
-					<input type="hidden" name="logintracks_clear" value="true"/>
-					<input type="submit" name="logintracks_submit" value="Clean login data"/>
-					<input type="hidden" name="update_nonce" value="<?php echo wp_create_nonce('lo_clear');?>" />
-			</form>
-			<!-- ###clear records### -->		
-			
-			
-			
-			
-			
-			
-			<!-- ENABLE/DISABLE OPTIONS -->
+		<div class="my_login_tracks"><!-- ENABLE/DISABLE OPTIONS -->
 			<?php
 			//IF whitelist updated
 			if (!empty($_POST['whitelist_ips'])) 
@@ -258,8 +206,7 @@ class Login_Restrict_logs
 			$d3 = $whiteip_answer == 3 ? "checked" : '';
 			$d2 = $whiteip_answer == 2 ? "checked" : '';
 			$d1 = $whiteip_answer == 1 || empty($whiteip_answer) ? "checked" : '';
-			?>
-			<br/><br/>	
+			?>	
 			<form method="post" action="">
 				<p class="submit">
 					<!--
@@ -273,7 +220,7 @@ class Login_Restrict_logs
 					-->
 					
 					
-					<div class="white_list_ipps" style="background-color: #1EE41E;padding: 5px; margin:0 0 0 20%;width: 50%;">
+					<div class="white_list_ipps" style="background-color: #1EE41E;padding: 5px; margin:0 0 0 10%;width: 50%;">
 						<div style="font-size:1.2em;font-weight:bold;">
 							IP WHITELISTING setting: (<a href="javascript:alert('1) OFF - do nothing (no restriction to unknown IPS and no notifications).\r\n2) get MAIL NOTIFICATION (if your server supports mailsending) at <?php echo get_option('admin_email');?> (address is changeable from Settings>General) when anyone logins, whose IP is not in this list. \r\n3) Block anyone to access LOGIN page at all [whose IP is not in the list]. \r\r\n(DONT FORGET TO INSERT YOUR IP TOO! HOWEVER,IF YOU BLOCK YOURSELF,enter your wordpress directory (from FTP) and add your IP into this file: /ALLOWED_IP/ . otherwise delete this plugin.)\r\n');">read more!!</a>):
 						</div>
@@ -303,11 +250,15 @@ class Login_Restrict_logs
 						}
 						lg_radiod();
 						</script>
+						
+						<div style="clear:both;"></div>
+						<input type="submit"  value="SAVE" onclick="return foo();" />
+						<input type="hidden" name="update_ips" value="<?php echo wp_create_nonce('lo_upd');?>" />
+						<br/>p.s. This plugin is good with <b>login attempt blockers</b>, like "Brute force login protection" or "Login Protection".
+						<br/>
 					</div>
 
-					<br/><div style="clear:both;"></div>
-					<input type="submit"  value="SAVE" onclick="return foo();" />
-					<input type="hidden" name="update_ips" value="<?php echo wp_create_nonce('lo_upd');?>" />
+					
 					<script type="text/javascript">
 					function foo()
 					{
@@ -328,14 +279,39 @@ class Login_Restrict_logs
 				</p> 
 			</form>
 			
+			<br/>
+			<br/>
+			
+			
+			<h2>All logins:</h2>
+			<table class="widefat" cellpadding="3" cellspacing="3"><tr><th>Username</th><th>Time(server)</th><th>IP</th><th>COUNTRY (<a href="javascript:alert('This is just an approximate country name. To view the full info for a particular IP, then in this column, click that COUNTRY NAME and you will be redirected to the WHOIS WEBSITE, where you will see the FULL INFORMATION of that IP.');">Read THIS!!</a>)</th><th>Succeed</th></tr>
+			<?php
+			if ($results){
+				foreach ($results as $e) {
+						if(!empty($e->country))	 {$countryyy =  '<a href="'. $this->whois_site . $e->IP.'" target="_blank"> '.$e->country.'</a>';}
+						else 					 {$countryyy =  '<a href="'. $this->whois_site . $e->IP.'" target="_blank"> problem_54_from_plugin</a>';}
+		
+					echo '<tr class="succeed"><td>'.$e->username.'</td><td>'.$e->time.'</td><td>'.$e->IP.'</td><td>'.$countryyy.'</td><td>succeed<td></tr>';
+				}
+			}
+			?>
+			</table>
+			
+			
+			<!-- clear records -->
+			<form method="post" action="">
+					<input type="hidden" name="logintracks_clear" value="true"/>
+					<input type="submit" name="logintracks_submit" value="Clean login data"/>
+					<input type="hidden" name="update_nonce" value="<?php echo wp_create_nonce('lo_clear');?>" />
+			</form>
+			<!-- ###clear records### -->		
 			
 			
 			
-			<br/><br/>
-
-			<div class="">
-			-This plugin is good with <b>login attempt blockers</b>, like "Brute force login protection" or "Login Protection".
-			</div>
+			
+			
+			
+			
 		</div>
 		<?php
 	}
