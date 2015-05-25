cdbe346415d79360fa336322a4236d2f9fc13a7c
selnomeria <selnomeria@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-25 15:26:26 +0000

    minor update
    git-svn-id: https://plugins.svn.wordpress.org/login-tracker-logs/trunk@1167187 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ip_country_data_2014/sample-test.php
+++ ip_country_data_2014/sample-test.php
@@ -1,5 +1,5 @@
 <?php
-include( dirname(__file__)."/geoip.inc" );
+if (!class_exists('GeoIP')){include_once( dirname(__file__)."/geoip.inc" );}
 $gi = geoip_open(dirname(__file__)."/GeoIP.dat", GEOIP_STANDARD); 
 $country_name = geoip_country_name_by_addr($gi, $user_ip );
 geoip_close($gi);
